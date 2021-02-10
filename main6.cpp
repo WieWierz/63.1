6ea74a8f08d9525cdf5c8d25d4d8a7be22fac5ed
WieWierz <62114839+WieWierz@users.noreply.github.com>
2021-02-10 09:51:45 +0100

    Update main6.cpp

--- main6.cpp
+++ main6.cpp
@@ -14,8 +14,8 @@ int main(int argc, char** argv) {
 	while(!plik1.eof()){
 	plik1>>liczba;
 	s = to_string(liczba);
-	string w1 = s.substr(0, s.length() / 2);
-	string w2 = s.substr(s.length() / 2);
+	string s1 = s.substr(0, s.length() / 2);
+	string s2 = s.substr(s.length() / 2);
 	if(w1==w2){
 		cout<<liczba<<endl;
 	}
