515a8628e235764e51c9c505abbaee292fcd0c9d
WieWierz <62114839+WieWierz@users.noreply.github.com>
2021-02-10 09:51:59 +0100

    Update main6.cpp

--- main6.cpp
+++ main6.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
 	s = to_string(liczba);
 	string s1 = s.substr(0, s.length() / 2);
 	string s2 = s.substr(s.length() / 2);
-	if(w1==w2){
+	if(s1==s2){
 		cout<<liczba<<endl;
 	}
 	}
