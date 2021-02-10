caee08d0e470073b74a0b8cd5440e709e3026baf
WieWierz <62114839+WieWierz@users.noreply.github.com>
2021-02-10 09:44:33 +0100

    Add files via upload

---
+++ main6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	ifstream plik1;
+	plik1.open("ciagi.txt");
+	int liczba;
+	string s;
+	
+	while(!plik1.eof()){
+	plik1>>liczba;
+	cout<<liczba;
+	s = to_string(liczba);
+	string w1 = s.substr(0, s.length() / 2);
+	string w2 = s.substr(s.length() / 2);
+	if(w1==w2){
+		cout<<liczba;
+	}
+	}
+	return 0;
+}
